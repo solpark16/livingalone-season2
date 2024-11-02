a58d65f9443cc39039ed159bfd4ee93c33723318
yuna-hh <ghddbsk12@naver.com>
2024-11-02 18:18:57 +0900

    모바일 header 수정

--- src/components/common/header/SearchBar.tsx
+++ src/components/common/header/SearchBar.tsx
@@ -3,18 +3,19 @@ import Image from "next/image";
 function SearchBar() {
   return (
     <>
-      <form className="flex flex-row w-[186px] px-[10px] py-[6px] border border-gray-3 rounded-full text-[15px]">
-        <Image
-          src="/img/icon-search.svg"
-          alt="검색버튼"
-          width={20}
-          height={20}
-          className="flex-shrink-0 mr-[10px]"
-        />
+      <form className="flex flex-row w-[166px] md:w-[186px] px-[10px] py-[6px] md:border md:border-gray-3 text-[14px] md:text-[15px] bg-white rounded-full">
+        <div className="flex-shrink-0 relative w-4 md:w-5 aspect-square mr-[10px]">
+          <Image
+            src="/img/icon-search.svg"
+            alt="검색버튼"
+            fill
+            className="object-cover"
+          />
+        </div>
         <input
           type="text"
           placeholder="검색어를 입력하세요"
-          className="w-full outline-none text-[15px] text-gray-6"
+          className="w-full outline-none text-[14px] md:text-[15px] text-gray-6"
         />
       </form>
     </>
