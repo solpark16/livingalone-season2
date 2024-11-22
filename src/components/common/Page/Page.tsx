774ed0ccd8908d973eee8a28ed5a8f15961409ab
yuna-hh <ghddbsk12@naver.com>
2024-11-22 14:49:18 +0900

    Page 컴포넌트 className 프롭스 추가

--- src/components/common/Page/Page.tsx
+++ src/components/common/Page/Page.tsx
@@ -1,9 +1,19 @@
 import { PropsWithChildren } from "react";
 
-function Page({ children }: PropsWithChildren) {
+interface PageProps {
+  className?: string;
+}
+
+function Page({ children, className }: PropsWithChildren<PageProps>) {
   return (
-    <div className="container mx-auto max-w-full xl:max-w-[1200px] pt-[40px] md:pt-[70px] pb-[150px] md:pb-[250px] min-h-screen px-[12px] xl:px-0">
-      <div className="flex flex-col items-center justify-center">{children}</div>
+    <div
+      className={`container mx-auto max-w-full xl:max-w-[1200px] pt-[40px] md:pt-[70px] pb-[150px] md:pb-[250px] min-h-screen px-[12px] xl:px-0 ${
+        className || ""
+      }`}
+    >
+      <div className="flex flex-col items-center justify-center">
+        {children}
+      </div>
     </div>
   );
 }
