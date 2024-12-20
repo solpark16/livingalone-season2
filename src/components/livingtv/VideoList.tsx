3a4fdfd560b4080e7e9801109210c6aacc96f99e
yuna-hh <ghddbsk12@naver.com>
2024-12-20 16:48:46 +0900

    fix: 자취티비 PageTitle 추가 및 레이아웃 수정

--- src/components/livingtv/VideoList.tsx
+++ src/components/livingtv/VideoList.tsx
@@ -12,37 +12,35 @@ function VideoList() {
   if (isError) return <Error />;
 
   return (
-    <div className="flex flex-col items-center text-[26px] font-bold text-gray-6 ">
-      <h2>자취 티비</h2>
-      <div className="flex flex-col gap-[100px] mt-[50px] md:mt-[80px]">
-        {videoData.map((playlist) => (
-          <div key={playlist.playlistId}>
-            <div className="flex flex-row gap-1 mb-5 text-[22px]">
-              <h3>{playlist.playlistTitle}</h3>
-              <span>|</span>
-              <span>#어쩌고 #저쩌고</span>
-            </div>
-            <ul className="grid grid-cols-4 gap-[13px]">
-              {playlist.videos.map((video) => (
-                <li key={video.videoId}>
-                  <a href={video.videoUrl} target="_blank">
-                    <Image
-                      src={video.thumbnail}
-                      width={290}
-                      height={162}
-                      alt="영상 썸네일"
-                      className="rounded-lg"
-                    />
-                    <span className="mt-5 text-xl line-clamp-2">
-                      {video.title}
-                    </span>
-                  </a>
-                </li>
-              ))}
-            </ul>
+    <div className="flex flex-col gap-[100px] mt-[50px] md:mt-[80px] text-[26px] font-bold text-gray-6">
+      {videoData.map((playlist) => (
+        <div key={playlist.playlistId}>
+          <div className="flex flex-row gap-1 mb-5 text-xl md:text-[22px]">
+            <h3>{playlist.playlistTitle}</h3>
+            <span>|</span>
+            <span>#어쩌고 #저쩌고</span>
           </div>
-        ))}
-      </div>
+          <ul className="grid grid-cols-2 md:grid-cols-4 gap-[10px] md:gap-[13px] justify-center">
+            {playlist.videos.map((video) => (
+              <li key={video.videoId} className="w-[290px]">
+                <a href={video.videoUrl} target="_blank">
+                  <Image
+                    src={video.thumbnail}
+                    width={290}
+                    height={162}
+                    alt="영상 썸네일"
+                    className="rounded-lg"
+                  />
+
+                  <span className="mt-5 text-base md:text-xl line-clamp-2">
+                    {video.title}
+                  </span>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
