dfcb099825c25178cde0a37c71f18e4e072fc70a
yuna-hh <ghddbsk12@naver.com>
2025-01-06 18:04:11 +0900

    pull 병합

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,20 @@ const nextConfig = {
     loaderFile: "./src/utils/supabase-image-loader.js",
     formats: ["image/avif", "image/webp"],
   },
+
+  async headers() {
+    return [
+      {
+        source: "/_next/static/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
